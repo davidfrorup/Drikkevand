f26485373e404da532a7ed655fa2aa936d60499f
David Frørup <dafr1@elev.tec.dk>
2024-01-08 12:03:24 +0100

    Version 3. 08-01-2024 12:03

--- Drikkevand/FindFile.cs
+++ Drikkevand/FindFile.cs
@@ -16,23 +16,102 @@ namespace Drikkevand
             do
             {
                 for (int i = 0; i < files.Length; i++)
-            {
-                Console.WriteLine($"{i + 1}: {files[i]}");
-            }
+                {
+                    Console.WriteLine($"{i + 1}: {files[i]}");
+                }
 
-            Console.Write("Vælg en fil ud for nummer: ");
-            string userInput = Console.ReadLine();
+                Console.Write("Vælg en fil ud for nummer: ");
+                string userInput = Console.ReadLine();
 
 
-            
+
                 if (int.TryParse(userInput, out int selectedFileIndex) && selectedFileIndex > 0 && selectedFileIndex <= files.Length)
                 {
                     string selectedFilePath = files[selectedFileIndex - 1];
 
-                    // Read JSON content from the selected file
                     string jsonContent = File.ReadAllText(selectedFilePath);
 
-                    break;
+                    List<QuizQuestions> quizQuestions = JsonConvert.DeserializeObject<List<QuizQuestions>>(jsonContent);
+
+                    Console.Clear();
+
+                    foreach (QuizQuestions question in quizQuestions)
+                    {
+
+
+                        Console.WriteLine($"Spørgsmål {question.Number}: {question.Question}");
+
+                        //bool correctAnswer = false;
+
+                        //do
+                        //{
+
+
+
+                        //    for (int i = 0; i < question.Answers.Count; i++)
+                        //    {
+                        //        Console.WriteLine($"{i + 1}: {question.Answers[i]}");
+                        //    }
+
+                        //    Console.Write("Vælg et svar ud for nummer: ");
+                        //    string userAnswer = Console.ReadLine();
+
+                        //    if (int.TryParse(userAnswer, out int selectedAnswerIndex) && selectedAnswerIndex > 0 && selectedAnswerIndex <= question.Answers.Count)
+                        //    {
+
+                        //        correctAnswer = string.Equals(question.Answers[selectedAnswerIndex - 1], question.CorrectAnswer);
+
+                        //        if (correctAnswer)
+                        //        {
+                        //            Console.WriteLine($"{question.IsCorrect[0]}. Info: {question.Info}");
+                        //        }
+                        //        else
+                        //        {
+                        //            Console.Clear();
+                        //            Console.WriteLine($"{question.IsCorrect[1]} Prøv igen!");
+                        //        }
+
+                        //    }
+                        //} while (!correctAnswer);
+
+
+                        bool correctAnswer = false;
+
+                        do
+                        {
+
+                            for (int i = 0; i < question.Answers.Count; i++)
+                            {
+                                Console.WriteLine($"{i + 1}: {question.Answers[i]}");
+                            }
+
+                            Console.Write("Vælg et svar ud for nummer: ");
+                            string userAnswer = Console.ReadLine();
+
+
+                            if (userAnswer == question.CorrectAnswer)
+                            {
+                                Console.WriteLine($"\n{question.IsCorrect[0]} \n\nInfo: {question.Info}");
+                                correctAnswer = true;
+                                Console.WriteLine("\nTryk vilkårlig tast for at fortsætte.");
+                                Console.ReadKey();
+                                Console.Clear();
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine($"{question.IsCorrect[1]} Prøv igen!");
+                                Console.WriteLine(question.Question);
+                                
+                            }
+                        } while (!correctAnswer);
+
+
+
+
+                    }
+                    //break;
+
                 }
                 else
                 {
@@ -42,3 +121,4 @@ namespace Drikkevand
         }
     }
 }
+
